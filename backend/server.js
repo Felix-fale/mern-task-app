c907d5a158899eeebb90f53fa7272dd08051b75e
Felix-fale <falefelix23@gmail.com>
2023-04-04 01:53:40 +0000

    feat: 16 setup Postman and insomia for API testing

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// app.get("/", (req, res) => {
-//   res.send("Bonjour, monde !");
-// });
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
 
 app.listen(PORT, () => {
   console.log(
