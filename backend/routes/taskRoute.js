e84f82e51f8b1f9c330b5489232296e8ef1c45d9
Felix-fale <falefelix23@gmail.com>
2023-04-05 20:14:45 +0000

    feat: 29. Delete a Task (Copy 1)

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -4,11 +4,13 @@ const {
   createTask,
   getTasks,
   getTask,
+  deleteTask,
 } = require("../controllers/taskController");
 const router = express.Router();
 
 router.post("/api/tasks", createTask);
 router.get("/api/tasks", getTasks);
 router.get("/api/tasks/:id", getTask);
+router.delete("/api/tasks/:id", deleteTask);
 
 module.exports = router;
