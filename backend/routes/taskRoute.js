909d8cb1957d9b544706c98b01c2e6d859369091
Felix-fale <falefelix23@gmail.com>
2023-04-05 21:23:24 +0000

    feat: 32. Refactor out Routes (Copy 1)

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -9,10 +9,13 @@ const {
 } = require("../controllers/taskController");
 const router = express.Router();
 
-router.post("/api/tasks", createTask);
-router.get("/api/tasks", getTasks);
-router.get("/api/tasks/:id", getTask);
-router.delete("/api/tasks/:id", deleteTask);
-router.patch("/api/tasks/:id", updateTask);
+router.route("/").get(getTasks).post(createTask);
+router.route("/:id").get(getTask).delete(deleteTask).put(updateTask);
+
+// router.post("/", createTask);
+// router.get("/", getTasks);
+// router.get("/:id", getTask);
+// router.delete("/:id", deleteTask);
+// router.put("/:id", updateTask);
 
 module.exports = router;
