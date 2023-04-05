2da7d98ef53d8a47267b18cccde46df8ebba4395
Felix-fale <falefelix23@gmail.com>
2023-04-05 00:27:29 +0000

    feat: 18. Connect To MongoDB – Method 1 (Copy 1)

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
+const doteenv = require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connectDB");
 
 const app = express();
-
-const PORT = process.env.PORT || 5000;
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Home page");
 });
 
+connectDB();
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(
     `Le serveur est en cours d'exécution sur http://localhost:${PORT}`
