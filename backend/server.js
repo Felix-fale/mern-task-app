ddaf2ba23426510134150c34da52f827668ae34c
Felix-fale <falefelix23@gmail.com>
2023-04-05 14:24:15 +0000

    feat:  23. Middleware (Copy 1)

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,13 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
+const Task = require("./model/taskModel");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-// const logger = (req, res, next) => {
-//   console.log("Middleware ran");
-//   console.log(req.method);
-//   next();
-// };
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.get("/", (req, res) => {
@@ -20,8 +17,12 @@ app.get("/", (req, res) => {
 
 // Create a Task
 app.post("/api/tasks", async (req, res) => {
-  console.log(req.body);
-  res.send("Task created");
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
@@ -31,9 +32,13 @@ mongoose
   .then(() => {
     // console.log(`MongoDB Connected`);
     app.listen(PORT, () => {
-      console.log(
-        `Le serveur est en cours d'exécution sur http://localhost:${PORT}`
-      );
+      console.log(`Server running on port: ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
+
+// const logger = (req, res, next) => {
+//   console.log("Middleware ran");
+//   console.log(req.method);
+//   next();
+// };
